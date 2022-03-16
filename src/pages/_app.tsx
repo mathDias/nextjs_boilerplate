da19cb5db1ea067658705acf4d77de908e46d2b9
Matheus Dias <services@kdee.com.br>
2022-03-16 12:56:55 -0300

    PWA & jest fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>MathDias - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple boilerplate to work with TypeScript, React, NextJS and Styled Components"
